474919e1437e2a8fc61b74591645eabf68aecf57
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-09 13:06:58 +0600

    its too hard to connect Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './index.css'
 import Search from './components/search'
-
+const API_BASE_URL ='https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS ={
+  method : 'GET',
+  Headers :{
+    accept : 'application/json',
+    Authorization :`Bearer ${API_KEY}`
+  }
+}
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessege, setErrorMessege]=useState("");
+
+  const fetchMovies = async()=>{
+    try{
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if(!response.ok){
+        throw new Error('Failed to Fetch Movies'); 
+      }
+      const data = await response.json();
+      console.log(data);
+    }
+    catch(error){
+      console.log(`Error Detected ${error}`);
+      setErrorMessege("The Movie is Currently Not Available")
+    }
+  }
+
+  useEffect(()=>{
+      fetchMovies();
+  },[])
 
   return (
     <main>
@@ -13,8 +42,14 @@ function App() {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+        <section className='all-movies'>
+            <h2>All Movies</h2>
+            {errorMessege && <p className='text-red-500'>{errorMessege}</p>}
+        </section>
+        
       </div>
     </main>
   )
