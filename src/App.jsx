f800d11bafdfe494467ed43f8462566b70649017
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-09 11:41:26 +0600

    ok i am doing it

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import Search from './components/search'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <main>
       <div className='pattern'/>
       <div className='wrapper'>
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassle</h1>
         </header>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </div>
     </main>
   )
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search(props) {
+  return (
+    <div className='text-white text-3xl'>
+      Search
+    </div>
+  )
+}
+
+export default Search;
