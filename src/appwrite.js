99f87c2e05a444c5cc0fea3f982a6fa9de92f1d8
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-10 22:06:02 +0600

    ok its hard af

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client } from "appwrite";
+import { Client, Query } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
@@ -10,5 +10,25 @@ const client = new Client()
 
 const database = new Databases(client);
 export const updateSearchCount = async(searchTerm,movie)=>{
-    
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID,[Query.equal('searchTerm',searchTerm),])
+        if(result.documents.length > 0) {
+   const doc = result.documents[0];
+
+   await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+    count: doc.count + 1,
+   })
+  // 3. If it doesn't, create a new document with the search term and count as 1
+  } else {
+   await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+    searchTerm,
+    count: 1,
+    movie_id: movie.id,
+    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+   })
+  }
+    }
+    catch(error){
+        console.error(error);
+    }
 }
