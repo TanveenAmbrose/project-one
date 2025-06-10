9e34239b2d911eda3593566f027f9cb6cfbca192
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-10 22:13:10 +0600

    its hard

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,18 +1,24 @@
-import { Client, Query } from "appwrite";
+import { Client, Databases, ID, Query } from 'appwrite'
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
-.setEndpoint('https://cloud.appwrite.io/v1')
-.setProject(PROJECT_ID)
+  .setEndpoint('https://fra.cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID)
 
 const database = new Databases(client);
-export const updateSearchCount = async(searchTerm,movie)=>{
-    try{
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID,[Query.equal('searchTerm',searchTerm),])
-        if(result.documents.length > 0) {
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  // 1. Use Appwrite SDK to check if the search term exists in the database
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.equal('searchTerm', searchTerm),
+  ])
+
+  // 2. If it does, update the count
+  if(result.documents.length > 0) {
    const doc = result.documents[0];
 
    await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
@@ -27,8 +33,20 @@ export const updateSearchCount = async(searchTerm,movie)=>{
     poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
    })
   }
-    }
-    catch(error){
-        console.error(error);
-    }
+ } catch (error) {
+  console.error(error);
+ }
+}
+
+export const getTrendingMovies = async () => {
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.limit(5),
+    Query.orderDesc("count")
+  ])
+
+  return result.documents;
+ } catch (error) {
+  console.error(error);
+ }
 }
